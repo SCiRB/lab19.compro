e6eb43428bc105618ce821895ad54a24bb8a33a4
Lica <Tanapat_choeichomsri@cmu.ac.th>
2023-02-17 17:01:02 +0700

    first changed

--- lab19_1.cpp
+++ lab19_1.cpp
@@ -20,20 +20,42 @@ string toUpperStr(string x){
     return y;
 }
 
-void importDataFromFile(){
-
+void importDataFromFile(string filename, vector<string> &names, vector<int> &scores, vector<char> &grades){
+    ifstream source;
+    source.open(filename);
+    char text[200];
+    char format[200] = "%[^:]: %d %d %d";
+    char name[200];
+    int a,b,c;
+    while(getline(source,text)){
+        sscanf(text,format,name,&a,&b,&c);
+        names.push_back(name);
+        scores.push_back(a+b+c);
+        grades.push_back(score2grade(a+b+c));
+    }
 }
 
-void getCommand(){
-
+void getCommand(string &command, string &key){
+    cout << "Please input your command: ";
+    cin >> command >> key;
 }
 
-void searchName(){
-
+void searchName(vector<string> names, vector<int> scores, vector<char> grades, string key){
+    for(int i = 0; i < names.size(); i++){
+        if(key == toUpperStr(names[i])){
+            cout << names[i] << "'s score =" << scores[i] << "\n";
+            cout << names[i] << "'s grade =" << grades[i] << "\n";
+        }
+    }
 }
 
-void searchGrade(){
-
+void searchGrade(vector<string> names, vector<int> scores, vector<char> grades, string key){
+    for(int i = 0; i < names.size(); i++){
+        if(key.c_str() == toUpperStr(grades[i])){
+            cout << names[i] << "'s score =" << scores[i] << "\n";
+            cout << names[i] << "'s grade =" << grades[i] << "\n";
+        }
+    }
 }
 
 
